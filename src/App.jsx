59fe8105f042d01d72568385ec25ba0fbe02107e
Iryna R <deutsche.post15@gmail.com>
2024-10-04 21:43:46 +0200

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,22 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import ContactForm from './components/ContactForm/ContactForm'
-import ContactList from './components/ContactList/ContactList'
-import SearchBox from './components/SearchBox/SearchBox';
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 import { PiAddressBookTabsLight } from "react-icons/pi";
 
-
 function App() {
-  const contactData = [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]
-  
-  const [searchBox, setSearchBox] = useState('');
-  const [contacts, setContacts] = useState(()=>{
-    const savedContacts = window.localStorage.getItem("saved-contact");
-    return savedContacts ? JSON.parse(savedContacts) : contactData;
-  });
-
-  useEffect(()=>{
-    window.localStorage.setItem("saved-contact", JSON.stringify(contacts))
-  }, [contacts])
-
-  const addContact = (newContact) => {
-    setContacts(prev => {
-      return [...prev, newContact]
-    })
-  };
-
-  const deleteContact = (contactId) =>{
-    setContacts(prev => {
-      return prev.filter(contact => contact.id !== contactId)
-    })
-  }
-
-  const filterContactList = contacts.filter((contact) => contact.name.toLowerCase().includes(searchBox.toLowerCase()))
-
   return (
     <>
-      <div className='containerPhonebook'>
-        <h1 className = "title">Phonebook <PiAddressBookTabsLight size='30' /></h1>
-        <ContactForm onAdd={addContact}/>
-        <SearchBox value={searchBox} onFilter={setSearchBox} />
-        <ContactList dataListContact={filterContactList} onDelete={deleteContact}/>
-        {/* {contacts.length !== 0 ? (
-          <ContactList dataListContact={filterContactList} onDelete={deleteContact}/>
-          ) : (<p className={s.infoText}>Your phonebook is empty</p>)} */}
-          
+      <div className="containerPhonebook">
+        <h1 className="title">
+          Phonebook <PiAddressBookTabsLight size="30" />
+        </h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,31 @@
-import s from './Contact.module.css'
+import s from "./Contact.module.css";
 import { IoMdContact } from "react-icons/io";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
-const Contact = ({ data: { id, name, number }, onDelete }) => {
-    return (
-        <div className={s.contactContainer}>
-            <div className={s.contactData}>
-                <h2 className={s.contactName}><IoMdContact size='20'/> {name}</h2>
-                <p className={s.contactPhone}><FaPhoneSquareAlt size='20'/> {number}</p>
-            </div>
-            <button className={s.contactBtnDelete} onClick={() => onDelete(id)}><MdDelete size='20'/> Delete</button>
-        </div>
-    )
-}
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
+
+const Contact = ({ data: { id, name, number } }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={s.contactContainer}>
+      <div className={s.contactData}>
+        <h2 className={s.contactName}>
+          <IoMdContact size="20" /> {name}
+        </h2>
+        <p className={s.contactPhone}>
+          <FaPhoneSquareAlt size="20" /> {number}
+        </p>
+      </div>
+      <button
+        className={s.contactBtnDelete}
+        onClick={() => dispatch(deleteContact(id))}
+      >
+        <MdDelete size="20" /> Delete
+      </button>
+    </div>
+  );
+};
 
 export default Contact;
